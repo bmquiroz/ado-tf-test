467debbcce0cfde9720b5e9970a4daedc733eb8b
Boris Quiroz <bmquiroz@outlook.com>
2023-12-31 19:33:27 -0500

    ...

--- tf/plans/rcits-aws-cloud/main.tf
+++ tf/plans/rcits-aws-cloud/main.tf
@@ -34,7 +34,7 @@ module "asg-blue-green-deploy" {
   ssm_parameter_name          = "win-2019-v1"
   blue_template_name          = "rcits-blue-template-v1"
   asg_name                    = "rcits-blue-green-asg"
-  network_interfaces          = [{security_groups = "sgr-0acffb9f663db547f"}]
+  network_interfaces          = [{security_groups = ["sgr-0acffb9f663db547f"]}]
   # tagging_standard            =  {
   #                               "deployment"  = "sandbox"
   #                               "patch" = "yes"
