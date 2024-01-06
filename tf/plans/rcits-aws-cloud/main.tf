b3f887951ba915cf03b23c4c0a4e21c9e476faba
Boris Quiroz <bmquiroz@outlook.com>
2024-01-06 16:37:37 -0500

    ...

--- tf/plans/rcits-aws-cloud/main.tf
+++ tf/plans/rcits-aws-cloud/main.tf
@@ -27,9 +27,9 @@ module "asg-blue-green-deploy" {
   green_template_name         = "rcits-green-template"
   asg_name                    = "rcits-blue-green-asg"
   instance_type               = "t2.micro"
-  max_size                    = 2
-  min_size                    = 1
-  capacity                    = 2
+  max_size                    = 4
+  min_size                    = 2
+  capacity                    = 4
   network_interfaces          = [{security_groups = ["sg-01e52f58d04156a9c"]}]
   deployment_color            = "green"
   vpc_id                      = "vpc-03d790a49d55d25c2"
