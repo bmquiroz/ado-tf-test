93884dec8bb7d656137acd2eef7d42ef5eac75ff
Boris Quiroz <bmquiroz@outlook.com>
2024-01-30 11:45:52 -0500

    ...

--- tf/plans/rcits-aws-cloud/main.tf
+++ tf/plans/rcits-aws-cloud/main.tf
@@ -23,16 +23,16 @@ module "asg-blue-green-deploy" {
   aws_subnet_compute_id       = ["subnet-047543b5ae3b70ee4", "subnet-04cef5a7ff8e71563"]
   aws_key_pair                = "rcits-poc-key1"
   ssm_parameter_blue_name     = "win-2019-v1"
-  ssm_parameter_green_name    = "win-2019-v2"
+  ssm_parameter_green_name    = "win-2019-v3"
   blue_template_name          = "rcits-blue-template"
   green_template_name         = "rcits-green-template"
   asg_name                    = "rcits-blue-green-asg"
   instance_type               = "t2.micro"
-  max_size                    = 2
-  min_size                    = 1
-  capacity                    = 2
+  max_size                    = 4
+  min_size                    = 2
+  capacity                    = 4
   network_interfaces          = [{security_groups = ["sg-01e52f58d04156a9c"]}]
-  deployment_color            = "blue"
+  deployment_color            = "green"
   vpc_id                      = "vpc-03d790a49d55d25c2"
   alb_sg_id                   = "sg-01e52f58d04156a9c"
   tagging_standard            =  {
